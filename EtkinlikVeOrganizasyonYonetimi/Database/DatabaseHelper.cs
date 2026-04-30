7090cd72fb22b2e2083fb1182bcb3075be9a3835
Sema Dede <semadedee@gmail.com>
2026-04-30 13:54:42 +0300

    feat: veritabanı bağlantı katmanı ve kullanıcı sorguları eklendi

---
+++ EtkinlikVeOrganizasyonYonetimi/Database/DatabaseHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtkinlikVeOrganizasyonYonetimi.Database
+{
+    public static class DatabaseHelper
+    {
+
+        // App.config'deki connection string'i okur
+        public static string ConnectionString =>
+            ConfigurationManager.ConnectionStrings["EtkinlikDB"].ConnectionString;
+
+        // Veritabanına bağlantı nesnesi döndürür
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+
+    }
+}
---
+++ EtkinlikVeOrganizasyonYonetimi/Database/KullaniciRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using EtkinlikVeOrganizasyonYonetimi.Models;
+
+namespace EtkinlikVeOrganizasyonYonetimi.Database
+{
+    public class KullaniciRepository
+    {
+        // Kullanıcı adı ve şifreyle giriş kontrolü yapar
+        // Doğruysa Kullanici nesnesini döndürür, yanlışsa null döner
+        public Kullanici GirisYap(string kullaniciAdi, string sifre)
+        {
+            using (SqlConnection conn = DatabaseHelper.GetConnection())
+            {
+                conn.Open();
+
+                string sql = "SELECT * FROM Kullanicilar WHERE KullaniciAdi = @KullaniciAdi AND Sifre = @Sifre";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@KullaniciAdi", kullaniciAdi);
+                cmd.Parameters.AddWithValue("@Sifre", sifre);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    return new Kullanici
+                    {
+                        KullaniciId = (int)reader["KullaniciId"],
+                        KullaniciAdi = reader["KullaniciAdi"].ToString(),
+                        Rol = reader["Rol"].ToString()
+                    };
+                }
+
+                return null;
+            }
+        }
+    }
+}
