cf949e59198ed98b39b147477bd25d1d9d31bbf7
Sema Dede <semadedee@gmail.com>
2026-04-30 14:23:22 +0300

    feat: login formu tamamlandı ve veritabanı bağlantısı doğrulandı

---
+++ EtkinlikVeOrganizasyonYonetimi/Forms/LoginForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+using EtkinlikVeOrganizasyonYonetimi.Database;
+using EtkinlikVeOrganizasyonYonetimi.Models;
+
+namespace EtkinlikVeOrganizasyonYonetimi.Forms
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            string kullaniciAdi = txtKullaniciAdi.Text.Trim();
+            string sifre = txtSifre.Text.Trim();
+
+            if (string.IsNullOrEmpty(kullaniciAdi) || string.IsNullOrEmpty(sifre))
+            {
+                MessageBox.Show("Kullanıcı adı ve şifre boş bırakılamaz.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            KullaniciRepository repo = new KullaniciRepository();
+            Kullanici kullanici = repo.GirisYap(kullaniciAdi, sifre);
+
+            if (kullanici == null)
+            {
+                MessageBox.Show("Kullanıcı adı veya şifre hatalı.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            MessageBox.Show($"Hoş geldiniz, {kullanici.KullaniciAdi}!", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Hide();
+        }
+    }
+}
--- EtkinlikVeOrganizasyonYonetimi/Program.cs
+++ EtkinlikVeOrganizasyonYonetimi/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EtkinlikVeOrganizasyonYonetimi.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace EtkinlikVeOrganizasyonYonetimi
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
